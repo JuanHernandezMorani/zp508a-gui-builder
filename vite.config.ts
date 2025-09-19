6088e0f3ec27cd2fd4952808e8fb77aad4175562
Juan Braian Hernandez Morani <juan.hernandez.morani@gmail.com>
2025-09-19 16:08:26 -0300

    fixing

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import path from "path"
 
 export default defineConfig({
   plugins: [react()],
-  root: './src/renderer',
-  base: './',
-  build: { outDir: '../../dist', emptyOutDir: true },
-  server: { port: 5173 }
+  root: "./src/renderer",
+  base: "./",
+  build: { outDir: "../../dist", emptyOutDir: true },
+  server: { port: 5173 },
+  resolve: {
+    alias: {
+      "@common": path.resolve(__dirname, "src/common"),
+    },
+  },
 })
